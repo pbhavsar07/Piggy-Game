9a35fdf3a351b82c9a9ec79d2a84129deabe0398
pbhavsar07 <32389003+pbhavsar07@users.noreply.github.com>
2018-01-25 23:49:17 +0530

    Update challenges.js

--- Piggy-Game/challenges.js
+++ Piggy-Game/challenges.js
@@ -1,40 +1,4 @@
-/*
-GAME RULES:
 
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-
-/*
- -do something of final value
- when roll dice is called - 
- i) ek element show karu dark round a final  score which is hidden initially
- ii) as soon as roll dice is called  - it is check if it has any number on it.
-    store that in that hidden element.
-iii) as hide our final scoreinput element and show the round wala final score lemeent
- 
-//write function for taking final value 
-//and call that from init only.
-//document.querySelector('#final-value').classList.add('nodisp');
-document.querySelector('#show-value').classList.add('disp');
-var x = document.querySelector('#show-value').textContent;
-console.log(x);
-
-
-
-YOUR 3 CHALLENGES
-Change the game to follow these rules:
-
-1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
-2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
-3. Add another dice to the game, so that there are two dices now. 
-The player looses his current score when one of them is a 1. 
-(Hint: you will need CSS to position the second dice, 
-    so take a look at the CSS code for the first one.)
-*/
 var scores, activePlayer,roundScore,gamePlaying;
 
 init();
@@ -62,6 +26,20 @@ function btn(){
 document.querySelector('.btn-roll').addEventListener('click',function(){
 //    console.log('Anonymous Function type; no name to this function; one time function');
 
+
+var x = document.querySelector('.winning-value').value;
+document.querySelector('.winning-value').readOnly = true;
+
+if(isNaN(x))
+{
+    console.log('final value entered is not a number; it is a string');
+    document.querySelector('.winning-value').value = 50;
+}
+else if(x == "")
+{
+    console.log('inpuy empty');
+    document.querySelector('.winning-value').value = 50;
+}
 
     if(gamePlaying)
     {
@@ -168,6 +146,8 @@ function init()
     activePlayer = 0;    
     gamePlaying = 1;
     document.querySelector('#show-value').classList.add('nodisp');
+    document.querySelector('.winning-value').readOnly = false;
+    document.querySelector('.winning-value').value = "";
 
     document.querySelector('#dice-0').style.display = 'none';
     document.querySelector('#dice-1').style.display = 'none';
